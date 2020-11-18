498db792fdf20259073abe9d87a3a37b12dbb95f
puttzon <puttzon@gmail.com>
2020-11-18 14:46:55 +0100

    lagt in kommentarer på alla plugins samt skapat funktion för väder

--- wp-content/plugins/form/form.php
+++ wp-content/plugins/form/form.php
@@ -1,6 +1,9 @@
 <?php
-// Plugin Name: Form
-//Author: Putte
-// Description: * Detta plugin skall kunna skriva ut ett kontaktformulär. Vid inskickat svar så sparas svaret i en lista som går att nå ifrån wp admin.
- //(Ungefär som pluginet Contactform7 med Flamingo.)
- //Tips: Formulär/ajax/register_posttype/insert_post/post_meta
+
+/** 
+ * Plugin Name: Form
+ * Author: Putte
+ * Description: * Detta plugin skall kunna skriva ut ett kontaktformulär. Vid inskickat svar så sparas svaret i en lista som går att nå ifrån wp admin.
+ * (Ungefär som pluginet Contactform7 med Flamingo.)
+ * Tips: Formulär/ajax/register_posttype/insert_post/post_meta
+ */
--- wp-content/plugins/weatherdata/weatherdata.php
+++ wp-content/plugins/weatherdata/weatherdata.php
@@ -1,7 +1,31 @@
 <?php
-// Plugin Name: Weatherdata
-// Author: Putte
-// Description:  Ett plugin som hämtar väderdata för vald plats, som presenteras med lämpliga      bilder och färger.
-  //Detta plugin har en inställningssida med minst fem olika alternativ för var på sidan det ska placeras:
-  //Produktsida, shop-sida, cart, checkout eller på en specifik produkt.
-  //Se till att cachea den hämtade datan så att inte api:et tillfrågas mer än ca en gång per timme, oavsett hur många sidträffar som sker.
+
+/**
+ * Plugin Name: Weatherdata
+ * Author: Putte
+ * Description:  Ett plugin som hämtar väderdata för vald plats, som presenteras med lämpliga      bilder och färger.
+ * Detta plugin har en inställningssida med minst fem olika alternativ för var på sidan det ska placeras:
+ * Produktsida, shop-sida, cart, checkout eller på en specifik produkt.
+ * Se till att cachea den hämtade datan så att inte api:et tillfrågas mer än ca en gång per timme, oavsett hur många sidträffar som sker.
+ */
+
+
+//Funktionen för att hämta API
+
+function get_weather()
+{
+  $response = wp_remote_get('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=57.70716&lon=11.96679');
+  $body = wp_remote_retrieve_body($response);
+  $api_response = json_decode($body, true);
+  return $api_response['properties']['timeseries'][0]['data']['instant']['details'];
+}
+
+function print_weather($weatherdata)
+{
+  echo "<p>Dagens väder i Götlaborg</p>";
+  echo '<p>Temp: ' . $weatherdata['air_temperature'] . ' C</p>';
+  echo '<p>Vind: ' . $weatherdata['wind_speed'] . ' m/s</p>';
+}
+
+$wdata = get_weather();
+print_weather($wdata);
