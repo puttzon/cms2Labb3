e9366bea80beaa1aadb32302fa57f155f98e5229
puttzon <puttzon@gmail.com>
2020-11-26 08:46:58 +0100

    Försökt bygga test funktion

--- wp-content/plugins/addon/test.php
+++ wp-content/plugins/addon/test.php
@@ -13,3 +13,34 @@
  *Gör dessa anrop så att två stycken genererer "Lyckat test" och den sista genererer "Misslyckat test"
  * Anropen till testfunktionen körs endast om url-parametern "testrun" är satt till "yes"
  */
+
+
+class test_app
+{
+
+  public function test_function($string, $förväntat_returvärde)
+  {
+    $newtest = new counter();
+    $test = $newtest->is_seven_letters_long($string);
+
+    if ($test == $förväntat_returvärde) {
+      echo '<p>Lyckat test!</p>';
+    } else {
+      echo '<p class="error">Test misslyckades</p>';
+    }
+  }
+  public function __construct()
+  {
+    add_action('wp_head', [$this, 'testforsevenletter']);
+  }
+}
+$runtest = new test_app();
+//Får inte denna att fungera. Får varning: Warning: call_user_func_array() expects parameter 1 to be a valid callback, class 'test_app' does not have a method 'testforsevenletter' in /Applications/MAMP/htdocs/CMS2/labb3/wp-includes/class-wp-hook.php on line 287. Men jag har testat olika saker fram och tillbaka.
+?>
+
+<style>
+  .error {
+    color: red;
+    font-size: 1.5em;
+  }
+</style>
